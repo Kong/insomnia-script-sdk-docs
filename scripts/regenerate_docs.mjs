63a9467c0990e759830bc40b0af2d3e2fe9a2b9d
georgehe <hexxa@outlook.com>
2025-04-15 16:40:15 +0800

    chore: regenerate pages

--- scripts/regenerate_docs.mjs
+++ scripts/regenerate_docs.mjs
@@ -1,20 +1,23 @@
 #!/usr/bin/env zx
 
-const gotTempDir = await question('Please input a temp dir for cloning the Insomnia repo (/tmp): ');
-const tempDir = gotTempDir.trim() === '' ? '/tmp' : gotTempDir.trim(); 
+const tempDir = tmpdir('generate-insomnia-script-sdk-docs');
 const docsDir = await $`pwd`;
 
-echo(`Will clone Insomnia to "${tempDir}"`);
+console.log(chalk.blue(`Cloning Insomnia to "${tempDir}" to generate docs...`));
 cd(tempDir);
 await $`rm -rf ./insomnia`;
 await $`git clone https://github.com/Kong/insomnia.git`;
 cd('insomnia');
 const insoDir = await $`pwd`;
 
-echo('Generating docs...');
+console.log(chalk.blue('Generating docs...'));
+await $`cp ${docsDir}/typedoc.json ${insoDir}/packages/insomnia-scripting-environment`
 await $`npm i -D typedoc`;
 cd('packages/insomnia-scripting-environment');
-await $`npx typedoc`;
+await $`npx typedoc --options typedoc.json`;
 await $`cp -r ${insoDir}/packages/insomnia-scripting-environment/docs ${docsDir}/`
 
-echo('Done, please check "git status" in the insomnia-script-sdk-docs repo.');
+await $`rm -rf ${tempDir}`;
+console.log(chalk.blue(`${tempDir} is removed.`));
+
+console.log(chalk.green('Done, please check "git status" in the insomnia-script-sdk-docs repo.'));
